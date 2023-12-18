c6840467391cbb6e443248ed2566ec3b1a5b8fe8
Ath <seof622@naver.com>
2023-12-19 00:34:05 +0900

    Feat: Solve A+B-3 (10950) Problem

---
+++ Algorithm_Practice/src/main/kotlin/level3/A_plus_B.kt
@@ -0,0 +1,19 @@
+package level3
+
+fun main() {
+    val numTest = readLine()!!.toInt()
+
+    val resultArray = arrayOfNulls<Int>(numTest)
+
+    for (iter in 1..numTest) {
+        val twoNumber = readLine()!!.split(" ")
+        val num1 = twoNumber.get(0).toInt()
+        val num2 = twoNumber.get(1).toInt()
+
+        resultArray[iter - 1] = num1 + num2
+    }
+
+    for (iter in 1..numTest) {
+        println(resultArray[iter - 1])
+    }
+}
