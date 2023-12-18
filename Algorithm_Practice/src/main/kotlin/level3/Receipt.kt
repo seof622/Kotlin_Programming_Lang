0172aa896453632de7638d3f7809b02c50fe526c
Ath <seof622@naver.com>
2023-12-19 01:13:41 +0900

    Feat: Solve receipt (25304) Problem

---
+++ Algorithm_Practice/src/main/kotlin/level3/Receipt.kt
@@ -0,0 +1,22 @@
+package level3
+
+fun main() {
+    val inputTotalAccount = readLine()!!.toLong()
+    val numOfItems = readLine()!!.toInt()
+
+    var totalAccount = 0
+
+    for (iter in 1..numOfItems) {
+        val accountDetail = readLine()!!.split(" ")
+        val account = accountDetail.get(0).toInt()
+        val itemNumber = accountDetail.get(1).toInt()
+
+        totalAccount += (account * itemNumber)
+    }
+
+    if (totalAccount.toLong() == inputTotalAccount) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+}
