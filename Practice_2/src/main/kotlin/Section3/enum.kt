0795732a156006dbc55d086e47bf6696f651266b
Ath <seof622@naver.com>
2023-12-18 20:19:22 +0900

    Feat: Kotlin enum class Practice

---
+++ Practice_2/src/main/kotlin/Section3/enum.kt
@@ -0,0 +1,18 @@
+package Section3
+
+// 질문의 난이도를 특수 클래스인 enum class로 정의
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+class EnumQuestion<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+
+fun main() {
+    val question1 = EnumQuestion<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = EnumQuestion<Boolean>("The sky is green. True or False", false, Difficulty.EASY)
+    val question3 = EnumQuestion<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+}
